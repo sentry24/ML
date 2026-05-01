7fbb5c080b58416ac7c83f566b4a186f71ced270
Moksh Singh <mokshsingh24@gmail.com>
2026-05-01 22:05:29 +0530

    Update practical_09_svm.py

--- practical_09_svm.py
+++ practical_09_svm.py
@@ -1,6 +1,5 @@
 # ============================================================
 # Practical 9 — SVM Classification
-# Moksh Singh (230439) | BSc(Hons) CS, Section B | Sem VI
 # Dataset: load_breast_cancer() — Classification
 # ============================================================
 
